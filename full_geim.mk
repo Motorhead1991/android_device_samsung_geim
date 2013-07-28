e488c8ded102eeebbf932b355a6ccb78f2c6d8eb
Motorhead1991 <motor-head-1991@hotmail.com>
2013-07-28 13:50:21 -0700

    Fixing an error

--- full_geim.mk
+++ full_geim.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/geim/geim.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_geim
